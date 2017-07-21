84755044431b827b90c8ba702b5872622956acfe
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-07-21 11:49:49 +0900

    Refactor constants

--- modules/constant.js
+++ modules/constant.js
@@ -6,18 +6,15 @@
   /* api */
   const os = require("os");
 
-  /* constants */
-  const CHAR = "utf8";
-  const DIR_HOME = os.homedir();
-  const EXT_CHROME = "chromeExtension";
-  const EXT_WEB = "webExtension";
-  const INDENT = 2;
-  const IS_BE = os.endianness() === "BE";
-  const IS_LE = os.endianness() === "LE";
-  const IS_MAC = os.platform() === "darwin";
-  const IS_WIN = os.platform() === "win32";
-
   module.exports = {
-    CHAR, DIR_HOME, EXT_CHROME, EXT_WEB, INDENT, IS_BE, IS_LE, IS_MAC, IS_WIN,
+    CHAR: "utf8",
+    DIR_HOME: os.homedir(),
+    EXT_CHROME: "chromeExtension",
+    EXT_WEB: "webExtension",
+    INDENT: 2,
+    IS_BE: os.endianness() === "BE",
+    IS_LE: os.endianness() === "LE",
+    IS_MAC: os.platform() === "darwin",
+    IS_WIN = os.platform() === "win32",
   };
 }
