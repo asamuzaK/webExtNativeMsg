37574cee04f327f7c34a0de08dae637422100aec
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-07-29 22:35:50 +0900

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,16 +2,22 @@
 {
   const {describe, it} = require("mocha");
   const {assert} = require("chai");
-  const {CmdArgs, Input, Output, Setup} = require("../index");
-  const {IS_BE} = require("../modules/constant");
+  const {ChildProcess, CmdArgs, Input, Output, Setup} = require("../index");
+  const {DIR_HOME, IS_BE} = require("../modules/constant");
 
   /* ChildProcess */
-  // FIXME
-  /*
   describe("ChildProcess", () => {
     const childProcess = new ChildProcess();
+    it("should throw if given command is not executable", async () => {
+      await childProcess.spawn().catch(e => {
+        assert.equal(e.message, "null is not executable.");
+      });
+    });
+    // FIXME: add test for childProcess.spawn() which passes
+    /*
+    it("should pass", async () => {});
+    */
   });
-  */
 
   /* CmdArgs */
   describe("CmdArgs", () => {
@@ -78,57 +84,70 @@
     });
 
     it("should get null if chromeExtensionIds is not given", () => {
-      assert.strictEqual(setup.chromeExtensionIds, null);
+      assert.isNull(setup.chromeExtensionIds, null);
     });
 
     it("should get null if webExtensionIds is not given", () => {
-      assert.strictEqual(setup.webExtensionIds, null);
+      assert.isNull(setup.webExtensionIds, null);
     });
 
     it("should get null if callback is not given", () => {
-      assert.strictEqual(setup.callback, null);
+      assert.isNull(setup.callback, null);
     });
 
     /* setters */
-    it("should set hostDescription", () => {
+    it("should set hostDescription in given string", () => {
       setup.hostDescription = "My new host description";
       assert.strictEqual(setup.hostDescription, "My new host description");
     });
 
-    it("should set hostName", () => {
+    it("should set hostName in given string", () => {
       setup.hostName = "mynewhost";
       assert.strictEqual(setup.hostName, "mynewhost");
     });
 
-    it("should set mainScriptFile", () => {
+    it("should set mainScriptFile in given string", () => {
       setup.mainScriptFile = "main.js";
       assert.strictEqual(setup.mainScriptFile, "main.js");
     });
 
-    it("should set chromeExtensionIds", () => {
+    it("should set chromeExtensionIds in given array", () => {
       setup.chromeExtensionIds = ["chrome://abc"];
       assert.deepEqual(setup.chromeExtensionIds, ["chrome://abc"]);
     });
 
-    it("should set webExtensionIds", () => {
+    it("should set webExtensionIds in given array", () => {
       setup.webExtensionIds = ["myapp@webextension"];
       assert.deepEqual(setup.webExtensionIds, ["myapp@webextension"]);
     });
 
-    it("should set callback function", () => {
+    it("should set callback in given function", () => {
       const myCallback = a => a;
       setup.callback = myCallback;
-      assert.typeOf(setup.callback, "function");
+      assert.strictEqual(setup.callback.name, "myCallback");
     });
 
     /* methods */
-    // FIXME
-    /*
-    it("setConfigDir", () => {
+    it("should throw if setConfigDir dir is not given", () => {
+      try {
+        setup.setConfigDir();
+      } catch (e) {
+        assert.strictEqual(e.message, "Failed to normalize undefined");
+      }
     });
-    */
 
-    // FIXME
+    it("should throw if setConfigDir dir is not subdirectory of user's home",
+       () => {
+         try {
+           setup.setConfigDir("/foo/bar/");
+         } catch (e) {
+           assert.strictEqual(
+             e.message, `Config path is not sub directory of ${DIR_HOME}.`
+           );
+         }
+       });
+
+    // FIXME: add test for run()
     /*
     it("run", () => {
     });
