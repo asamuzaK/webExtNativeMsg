b9cbfba552f8c5d8955b76b1bfaecc911bb11c52
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-07-30 11:37:31 +0900

    Remove process

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,6 @@
   const fs = require("fs");
   const os = require("os");
   const path = require("path");
-  const process = require("process");
 
   /* constants */
   const DIR_HOME = os.homedir();
