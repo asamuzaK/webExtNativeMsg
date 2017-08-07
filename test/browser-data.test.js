1599ad99090fdbe7f13e30d6aca7b8dd0cfea538
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-08-07 23:35:58 +0900

    Update test

--- test/browser-data.test.js
+++ test/browser-data.test.js
@@ -5,37 +5,145 @@
   const {assert} = require("chai");
   const {describe, it} = require("mocha");
 
-  describe("Browser Data", () => {
-    it("should contain firefox property", async () => {
-      assert.property(browserData, "firefox");
+  describe("browserData", () => {
+    describe("Firefox", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "firefox");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.firefox, ["alias", "type"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.firefox,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain cyberfox property", async () => {
-      assert.property(browserData, "cyberfox");
+    describe("Cyberfox", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "cyberfox");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.cyberfox, ["alias", "type"]);
+      });
+
+      it("should contain alter alias key", async () => {
+        assert.hasAnyKeys(browserData.cyberfox, ["aliasMac", "aliasWin"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.cyberfox,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain waterfox property", async () => {
-      assert.property(browserData, "waterfox");
+    describe("Waterfox", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "waterfox");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.waterfox, ["alias", "type"]);
+      });
+
+      it("should contain alter alias key", async () => {
+        assert.hasAnyKeys(browserData.waterfox, ["aliasMac", "aliasWin"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.waterfox,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain chrome property", async () => {
-      assert.property(browserData, "chrome");
+    describe("Chrome", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "chrome");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.chrome, ["alias", "type"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.chrome,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain chromium property", async () => {
-      assert.property(browserData, "chromium");
+    describe("Chromium", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "chromium");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.chromium, ["alias", "type"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.chromium,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain kinza property", async () => {
-      assert.property(browserData, "kinza");
+    describe("Kinza", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "kinza");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.kinza, ["alias", "type"]);
+      });
+
+      it("should contain alter alias key", async () => {
+        assert.hasAnyKeys(browserData.kinza, ["aliasMac", "aliasWin"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.kinza,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain opera property", async () => {
-      assert.property(browserData, "opera");
+    describe("Opera", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "opera");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.opera, ["alias", "type"]);
+      });
+
+      it("should contain alter alias key", async () => {
+        assert.hasAnyKeys(browserData.opera, ["aliasMac", "aliasWin"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.opera,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
 
-    it("should contain vivaldi property", async () => {
-      assert.property(browserData, "vivaldi");
+    describe("Vivaldi", () => {
+      it("should contain property", async () => {
+        assert.property(browserData, "vivaldi");
+      });
+
+      it("should contain alias and type keys", async () => {
+        assert.containsAllKeys(browserData.vivaldi, ["alias", "type"]);
+      });
+
+      it("should contain alter alias key", async () => {
+        assert.hasAnyKeys(browserData.vivaldi, ["aliasMac", "aliasWin"]);
+      });
+
+      it("should contain host keys", async () => {
+        assert.hasAnyKeys(browserData.vivaldi,
+                          ["hostLinux", "hostMac", "regWin"]);
+      });
     });
   });
 }
