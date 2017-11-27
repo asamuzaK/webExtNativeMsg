d8be3fca1b56b574b85d5249ad5dbfabf2f69043
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-11-27 15:02:20 +0900

    Add tests for child process spawn

--- test/child-process.test.js
+++ test/child-process.test.js
@@ -1,22 +1,25 @@
-/* eslint-disable max-nested-callbacks */
+/* eslint-disable max-nested-callbacks, no-magic-numbers */
 "use strict";
 {
   /* api */
   const {ChildProcess, CmdArgs} = require("../modules/child-process");
   const {assert} = require("chai");
   const {describe, it} = require("mocha");
+  const sinon = require("sinon");
+  const childProcess = require("child_process");
   const fs = require("fs");
   const path = require("path");
 
   /* constants */
   const {IS_WIN} = require("../modules/constant");
   const PERM_EXEC = 0o700;
+  const PERM_FILE = 0o600;
 
   /* ChildProcess */
   describe("ChildProcess", () => {
     it("should create an instance", () => {
-      const childProcess = new ChildProcess();
-      assert.instanceOf(childProcess, ChildProcess);
+      const proc = new ChildProcess();
+      assert.instanceOf(proc, ChildProcess);
     });
 
     /* method */
@@ -49,6 +52,84 @@
         proc.on("close", code => {
           assert.strictEqual(code, 0);
         });
+      });
+
+      it("should spawn with file path as first argument", async () => {
+        const app = path.resolve(
+          IS_WIN && path.join("test", "file", "test.cmd") ||
+          path.join("test", "file", "test.sh")
+        );
+        const file = path.resolve(path.join("test", "file", "test.txt"));
+        await fs.chmodSync(app, PERM_EXEC);
+        await fs.chmodSync(file, PERM_FILE);
+        const stub = sinon.stub(childProcess, "spawn").callsFake((...args) => {
+          const [, cmdArgs] = args;
+          const [filePath] = cmdArgs;
+          assert.strictEqual(cmdArgs.length, 1);
+          assert.strictEqual(filePath, file);
+        });
+        await (new ChildProcess(app)).spawn(file);
+        stub.restore();
+      });
+
+      it("should spawn with file path as first argument", async () => {
+        const app = path.resolve(
+          IS_WIN && path.join("test", "file", "test.cmd") ||
+          path.join("test", "file", "test.sh")
+        );
+        const file = path.resolve(path.join("test", "file", "test.txt"));
+        await fs.chmodSync(app, PERM_EXEC);
+        await fs.chmodSync(file, PERM_FILE);
+        const stub = sinon.stub(childProcess, "spawn").callsFake((...args) => {
+          const [, cmdArgs] = args;
+          const [filePath] = cmdArgs;
+          assert.strictEqual(cmdArgs.length, 1);
+          assert.strictEqual(filePath, file);
+        });
+        await (new ChildProcess(app)).spawn(file, true);
+        stub.restore();
+      });
+
+      it("should spawn with file path as first argument", async () => {
+        const app = path.resolve(
+          IS_WIN && path.join("test", "file", "test.cmd") ||
+          path.join("test", "file", "test.sh")
+        );
+        const arg = ["-a", "-b"];
+        const file = path.resolve(path.join("test", "file", "test.txt"));
+        await fs.chmodSync(app, PERM_EXEC);
+        await fs.chmodSync(file, PERM_FILE);
+        const stub = sinon.stub(childProcess, "spawn").callsFake((...args) => {
+          const [, cmdArgs] = args;
+          const [arg1, arg2, arg3] = cmdArgs;
+          assert.strictEqual(cmdArgs.length, 3);
+          assert.strictEqual(arg1, file);
+          assert.strictEqual(arg2, "-a");
+          assert.strictEqual(arg3, "-b");
+        });
+        await (new ChildProcess(app, arg)).spawn(file);
+        stub.restore();
+      });
+
+      it("should spawn with file path as last argument", async () => {
+        const app = path.resolve(
+          IS_WIN && path.join("test", "file", "test.cmd") ||
+          path.join("test", "file", "test.sh")
+        );
+        const arg = ["-a", "-b"];
+        const file = path.resolve(path.join("test", "file", "test.txt"));
+        await fs.chmodSync(app, PERM_EXEC);
+        await fs.chmodSync(file, PERM_FILE);
+        const stub = sinon.stub(childProcess, "spawn").callsFake((...args) => {
+          const [, cmdArgs] = args;
+          const [arg1, arg2, arg3] = cmdArgs;
+          assert.strictEqual(cmdArgs.length, 3);
+          assert.strictEqual(arg1, "-a");
+          assert.strictEqual(arg2, "-b");
+          assert.strictEqual(arg3, file);
+        });
+        await (new ChildProcess(app, arg)).spawn(file, true);
+        stub.restore();
       });
     });
   });
