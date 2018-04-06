6bb5b63aee52fc515a25da33ec66fbdb7ed1a46a
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-04-07 02:35:48 +0900

    Refactor constant test

--- test/constant.test.js
+++ test/constant.test.js
@@ -3,11 +3,51 @@
   /* api */
   const {assert} = require("chai");
   const {describe, it} = require("mocha");
+  const {isString} = require("../modules/common");
 
   /* constant */
-  const {DIR_CONFIG, DIR_HOME, IS_WIN, IS_MAC} = require("../modules/constant");
+  const {
+    CHAR, DIR_CONFIG, DIR_HOME, EXT_CHROME, EXT_WEB, INDENT, IS_BE, IS_LE,
+    IS_MAC, IS_WIN,
+  } = require("../modules/constant");
 
-  /* OS specific user config dir */
+  describe("string constants", () => {
+    it("should get string", () => {
+      const arr = [
+        CHAR,
+        DIR_HOME,
+        EXT_CHROME,
+        EXT_WEB,
+      ];
+      arr.forEach(i => {
+        assert.isTrue(isString(i));
+      });
+    });
+  });
+
+  describe("number constants", () => {
+    it("should get number", () => {
+      const arr = [INDENT];
+      arr.forEach(i => {
+        assert.isTrue(typeof i === "number");
+      });
+    });
+  });
+
+  describe("boolean constants", () => {
+    it("should get boolean", () => {
+      const arr = [
+        IS_BE,
+        IS_LE,
+        IS_MAC,
+        IS_WIN,
+      ];
+      arr.forEach(i => {
+        assert.isTrue(typeof i === "boolean");
+      });
+    });
+  });
+
   describe("DIR_CONFIG", () => {
     it("should get value", () => {
       const dir = IS_WIN && [DIR_HOME, "AppData", "Roaming"] ||
