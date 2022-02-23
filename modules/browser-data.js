eca44666dc69e4fdf01326439a9ef8be09b08282
asamuzaK (Kazz) <kazz@asamuzak.jp>
2022-02-23 11:02:49 +0900

    Update constants

--- modules/browser-data.js
+++ modules/browser-data.js
@@ -9,84 +9,85 @@ import {
 
 /* constants */
 const HKCU_SOFTWARE = ['HKEY_CURRENT_USER', 'SOFTWARE'];
-const NM_HOSTS = 'NativeMessagingHosts';
+const HOST = 'native-messaging-hosts';
+const HOST_CAMEL = 'NativeMessagingHosts';
 
 /* browser config data */
 export const browserData = {
   /* gecko */
   firefox: {
     alias: 'firefox',
-    hostLinux: [DIR_HOME, '.mozilla', 'native-messaging-hosts'],
-    hostMac: [DIR_CONFIG_MAC, 'Mozilla', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Mozilla', NM_HOSTS],
+    hostLinux: [DIR_HOME, '.mozilla', HOST],
+    hostMac: [DIR_CONFIG_MAC, 'Mozilla', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Mozilla', HOST_CAMEL],
     type: EXT_WEB
   },
   thunderbird: {
     alias: 'thunderbird',
-    hostLinux: [DIR_HOME, '.thunderbird', 'native-messaging-hosts'],
-    hostMac: [DIR_CONFIG_MAC, 'Thunderbird', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Thunderbird', NM_HOSTS],
+    hostLinux: [DIR_HOME, '.thunderbird', HOST],
+    hostMac: [DIR_CONFIG_MAC, 'Thunderbird', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Thunderbird', HOST_CAMEL],
     type: EXT_WEB
   },
   waterfoxcurrent: {
     alias: 'waterfoxcurrent',
-    hostLinux: [DIR_HOME, '.waterfox', 'native-messaging-hosts'],
-    hostMac: [DIR_CONFIG_MAC, 'Waterfox', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Waterfox', NM_HOSTS],
+    hostLinux: [DIR_HOME, '.waterfox', HOST],
+    hostMac: [DIR_CONFIG_MAC, 'Waterfox', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Waterfox', HOST_CAMEL],
     type: EXT_WEB
   },
   librewolf: {
     alias: 'librewolf',
     aliasWin: 'firefox',
-    hostLinux: [DIR_HOME, '.librewolf', 'native-messaging-hosts'],
+    hostLinux: [DIR_HOME, '.librewolf', HOST],
     hostMac: null,
-    regWin: [...HKCU_SOFTWARE, 'Mozilla', NM_HOSTS],
+    regWin: [...HKCU_SOFTWARE, 'Mozilla', HOST_CAMEL],
     type: EXT_WEB
   },
   /* blink */
   chrome: {
     alias: 'chrome',
-    hostLinux: [DIR_CONFIG_LINUX, 'google-chrome', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'google-chrome', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   },
   chromebeta: {
     alias: 'chromebeta',
     aliasWin: 'chrome',
-    hostLinux: [DIR_CONFIG_LINUX, 'google-chrome-beta', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome Beta', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'google-chrome-beta', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome Beta', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   },
   chromecanary: {
     alias: 'chromecanary',
     aliasWin: 'chrome',
     hostLinux: null,
-    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome Canary', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome Canary', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   },
   chromium: {
     alias: 'chromium',
-    hostLinux: [DIR_CONFIG_LINUX, 'chromium', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'Chromium', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'chromium', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'Chromium', HOST_CAMEL],
     regWin: null,
     type: EXT_CHROME
   },
   brave: {
     alias: 'brave',
     aliasWin: 'chrome',
-    hostLinux: [DIR_CONFIG_LINUX, 'BraveSoftware', 'Brave-Browser', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'BraveSoftware', 'Brave-Browser', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'BraveSoftware', 'Brave-Browser', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'BraveSoftware', 'Brave-Browser', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   },
   edge: {
     alias: 'edge',
-    hostLinux: [DIR_CONFIG_LINUX, 'microsoft-edge', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'Microsoft Edge', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Microsoft', 'Edge', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'microsoft-edge', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'Microsoft Edge', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Microsoft', 'Edge', HOST_CAMEL],
     type: EXT_CHROME
   },
   opera: {
@@ -94,16 +95,16 @@ export const browserData = {
     aliasMac: 'chrome',
     aliasWin: 'chrome',
     hostLinux: null,
-    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostMac: [DIR_CONFIG_MAC, 'Google', 'Chrome', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   },
   vivaldi: {
     alias: 'vivaldi',
     aliasWin: 'chrome',
-    hostLinux: [DIR_CONFIG_LINUX, 'vivaldi', NM_HOSTS],
-    hostMac: [DIR_CONFIG_MAC, 'Vivaldi', NM_HOSTS],
-    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', NM_HOSTS],
+    hostLinux: [DIR_CONFIG_LINUX, 'vivaldi', HOST_CAMEL],
+    hostMac: [DIR_CONFIG_MAC, 'Vivaldi', HOST_CAMEL],
+    regWin: [...HKCU_SOFTWARE, 'Google', 'Chrome', HOST_CAMEL],
     type: EXT_CHROME
   }
 };
