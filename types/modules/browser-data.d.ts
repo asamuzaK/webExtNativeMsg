232ac2ecd235390fa71952542a0881c0dcc629e7
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-06-10 11:06:35 +0900

    Update browser-data.d.ts

--- types/modules/browser-data.d.ts
+++ types/modules/browser-data.d.ts
@@ -1,141 +1,141 @@
 export namespace browserData {
     namespace firefox {
-        export const alias: string;
-        export const hostLinux: string[];
-        export const hostMac: string[];
-        export const regWin: string[];
+        export let alias: string;
+        export let hostLinux: string[];
+        export let hostMac: string[];
+        export let regWin: string[];
         export { EXT_WEB as type };
     }
     namespace thunderbird {
-        const alias_1: string;
+        let alias_1: string;
         export { alias_1 as alias };
-        const hostLinux_1: string[];
+        let hostLinux_1: string[];
         export { hostLinux_1 as hostLinux };
-        const hostMac_1: string[];
+        let hostMac_1: string[];
         export { hostMac_1 as hostMac };
-        const regWin_1: string[];
+        let regWin_1: string[];
         export { regWin_1 as regWin };
         export { EXT_WEB as type };
     }
     namespace waterfoxcurrent {
-        const alias_2: string;
+        let alias_2: string;
         export { alias_2 as alias };
-        const hostLinux_2: string[];
+        let hostLinux_2: string[];
         export { hostLinux_2 as hostLinux };
-        const hostMac_2: string[];
+        let hostMac_2: string[];
         export { hostMac_2 as hostMac };
-        const regWin_2: string[];
+        let regWin_2: string[];
         export { regWin_2 as regWin };
         export { EXT_WEB as type };
     }
     namespace librewolf {
-        const alias_3: string;
+        let alias_3: string;
         export { alias_3 as alias };
-        export const aliasWin: string;
-        const hostLinux_3: string[];
+        export let aliasWin: string;
+        let hostLinux_3: string[];
         export { hostLinux_3 as hostLinux };
-        const hostMac_3: any;
+        let hostMac_3: any;
         export { hostMac_3 as hostMac };
-        const regWin_3: string[];
+        let regWin_3: string[];
         export { regWin_3 as regWin };
         export { EXT_WEB as type };
     }
     namespace chrome {
-        const alias_4: string;
+        let alias_4: string;
         export { alias_4 as alias };
-        const hostLinux_4: string[];
+        let hostLinux_4: string[];
         export { hostLinux_4 as hostLinux };
-        const hostMac_4: string[];
+        let hostMac_4: string[];
         export { hostMac_4 as hostMac };
-        const regWin_4: string[];
+        let regWin_4: string[];
         export { regWin_4 as regWin };
         export { EXT_CHROME as type };
     }
     namespace chromebeta {
-        const alias_5: string;
+        let alias_5: string;
         export { alias_5 as alias };
-        const aliasWin_1: string;
+        let aliasWin_1: string;
         export { aliasWin_1 as aliasWin };
-        const hostLinux_5: string[];
+        let hostLinux_5: string[];
         export { hostLinux_5 as hostLinux };
-        const hostMac_5: string[];
+        let hostMac_5: string[];
         export { hostMac_5 as hostMac };
-        const regWin_5: string[];
+        let regWin_5: string[];
         export { regWin_5 as regWin };
         export { EXT_CHROME as type };
     }
     namespace chromecanary {
-        const alias_6: string;
+        let alias_6: string;
         export { alias_6 as alias };
-        const aliasWin_2: string;
+        let aliasWin_2: string;
         export { aliasWin_2 as aliasWin };
-        const hostLinux_6: any;
+        let hostLinux_6: any;
         export { hostLinux_6 as hostLinux };
-        const hostMac_6: string[];
+        let hostMac_6: string[];
         export { hostMac_6 as hostMac };
-        const regWin_6: string[];
+        let regWin_6: string[];
         export { regWin_6 as regWin };
         export { EXT_CHROME as type };
     }
     namespace chromium {
-        const alias_7: string;
+        let alias_7: string;
         export { alias_7 as alias };
-        const hostLinux_7: string[];
+        let hostLinux_7: string[];
         export { hostLinux_7 as hostLinux };
-        const hostMac_7: string[];
+        let hostMac_7: string[];
         export { hostMac_7 as hostMac };
-        const regWin_7: any;
+        let regWin_7: any;
         export { regWin_7 as regWin };
         export { EXT_CHROME as type };
     }
     namespace brave {
-        const alias_8: string;
+        let alias_8: string;
         export { alias_8 as alias };
-        const aliasWin_3: string;
+        let aliasWin_3: string;
         export { aliasWin_3 as aliasWin };
-        const hostLinux_8: string[];
+        let hostLinux_8: string[];
         export { hostLinux_8 as hostLinux };
-        const hostMac_8: string[];
+        let hostMac_8: string[];
         export { hostMac_8 as hostMac };
-        const regWin_8: string[];
+        let regWin_8: string[];
         export { regWin_8 as regWin };
         export { EXT_CHROME as type };
     }
     namespace edge {
-        const alias_9: string;
+        let alias_9: string;
         export { alias_9 as alias };
-        const hostLinux_9: string[];
+        let hostLinux_9: string[];
         export { hostLinux_9 as hostLinux };
-        const hostMac_9: string[];
+        let hostMac_9: string[];
         export { hostMac_9 as hostMac };
-        const regWin_9: string[];
+        let regWin_9: string[];
         export { regWin_9 as regWin };
         export { EXT_CHROME as type };
     }
     namespace opera {
-        const alias_10: string;
+        let alias_10: string;
         export { alias_10 as alias };
-        export const aliasMac: string;
-        const aliasWin_4: string;
+        export let aliasMac: string;
+        let aliasWin_4: string;
         export { aliasWin_4 as aliasWin };
-        const hostLinux_10: any;
+        let hostLinux_10: any;
         export { hostLinux_10 as hostLinux };
-        const hostMac_10: string[];
+        let hostMac_10: string[];
         export { hostMac_10 as hostMac };
-        const regWin_10: string[];
+        let regWin_10: string[];
         export { regWin_10 as regWin };
         export { EXT_CHROME as type };
     }
     namespace vivaldi {
-        const alias_11: string;
+        let alias_11: string;
         export { alias_11 as alias };
-        const aliasWin_5: string;
+        let aliasWin_5: string;
         export { aliasWin_5 as aliasWin };
-        const hostLinux_11: string[];
+        let hostLinux_11: string[];
         export { hostLinux_11 as hostLinux };
-        const hostMac_11: string[];
+        let hostMac_11: string[];
         export { hostMac_11 as hostMac };
-        const regWin_11: string[];
+        let regWin_11: string[];
         export { regWin_11 as regWin };
         export { EXT_CHROME as type };
     }
