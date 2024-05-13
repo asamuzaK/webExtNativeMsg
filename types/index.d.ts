b59b3753b997380034e1a92024e538504a82613f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-05-13 23:43:17 +0900

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
+export { ChildProcess } from "./modules/child-process.js";
 export { Setup } from "./modules/setup.js";
-export { ChildProcess, CmdArgs } from "./modules/child-process.js";
 export { Input, Output } from "./modules/native-message.js";
 export { convertUriToFilePath, createDirectory, createFile, getAbsPath, getFileNameFromFilePath, getFileTimestamp, getStat, isDir, isExecutable, isFile, isSubDir, readFile, removeDir, removeDirectory } from "./modules/file-util.js";
