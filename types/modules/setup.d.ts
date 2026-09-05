eb3a4b67178919a7063fa7ef4c91ef8922e7514d
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-09-05 11:44:03 +0900

    v9.0.5

--- types/modules/setup.d.ts
+++ types/modules/setup.d.ts
@@ -2,9 +2,30 @@ import { Separator } from '@inquirer/prompts';
 export declare const inquirer: {
     confirm: import("@inquirer/type").Prompt<boolean, {
         message: string;
-        default?: boolean;
+        default?: boolean | undefined | undefined;
+        transformer?: ((value: boolean) => string) | undefined;
+        theme?: import("@inquirer/type").PartialDeep<import("@inquirer/core").Theme<{
+            keywords: {
+                yes: string;
+                no: string;
+            };
+            style: {
+                confirmDefault: (text: string) => string;
+            };
+        }>> | undefined;
+    } & {
+        message: string;
+        default?: boolean | undefined;
         transformer?: (value: boolean) => string;
-        theme?: import("@inquirer/type").PartialDeep<import("@inquirer/core").Theme>;
+        theme?: import("@inquirer/type").PartialDeep<import("@inquirer/core").Theme<{
+            keywords: {
+                yes: string;
+                no: string;
+            };
+            style: {
+                confirmDefault: (text: string) => string;
+            };
+        }>>;
     }>;
     select: <const Value>(config: {
         message: string;
@@ -33,6 +54,33 @@ export declare const inquirer: {
             };
             indexMode: 'hidden' | 'number';
         }>> | undefined;
+    } & {
+        message: string;
+        choices: readonly (Value | {
+            value: Value;
+            name?: string;
+            description?: string;
+            short?: string;
+            disabled?: boolean | string;
+            type?: never;
+        } | Separator)[];
+        pageSize?: number;
+        loop?: boolean;
+        default?: NoInfer<Value> | undefined;
+        theme?: import("@inquirer/type").PartialDeep<import("@inquirer/core").Theme<{
+            icon: {
+                cursor: string;
+            };
+            style: {
+                disabled: (text: string) => string;
+                description: (text: string) => string;
+                keysHelpTip: (keys: [key: string, action: string][]) => string | undefined;
+            };
+            i18n: {
+                disabledError: string;
+            };
+            indexMode: 'hidden' | 'number';
+        }>>;
     }, context?: import("@inquirer/type").Context) => Promise<Value>;
 };
 export declare const handleInquirerError: (e: object) => never;
